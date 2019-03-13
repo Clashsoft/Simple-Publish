f90d2211173d1201f80d1e2305636e3d7db4e370
Adrian Kunz <clashsoft@hotmail.com>
2019-03-13 15:30:41 +0100

    Creates sourcesJar and javadocJar

--- src/main/groovy/de/clashsoft/simplepublish/SimplePublishPlugin.groovy
+++ src/main/groovy/de/clashsoft/simplepublish/SimplePublishPlugin.groovy
@@ -2,10 +2,19 @@ package de.clashsoft.simplepublish
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.bundling.Jar
 
 class SimplePublishPlugin implements Plugin<Project> {
 	@Override
 	void apply(Project target) {
+		target.task('sourcesJar', type: Jar) {
+			from sourceSets.main.allSource
+			classifier = 'sources'
+		}
 
+		target.task('javadocJar', type: Jar, dependsOn: 'javadoc') {
+			from javadoc.destinationDir
+			classifier = 'javadoc'
+		}
 	}
 }
