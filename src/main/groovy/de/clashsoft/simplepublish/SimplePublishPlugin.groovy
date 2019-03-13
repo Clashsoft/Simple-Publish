bf17eccd3ee1c903313543d4e60769041907ba0d
Adrian Kunz <clashsoft@hotmail.com>
2019-03-13 15:25:37 +0100

    Add SimplePublishPlugin class

---
+++ src/main/groovy/de/clashsoft/simplepublish/SimplePublishPlugin.groovy
@@ -0,0 +1,11 @@
+package de.clashsoft.simplepublish
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SimplePublishPlugin implements Plugin<Project> {
+	@Override
+	void apply(Project target) {
+
+	}
+}
