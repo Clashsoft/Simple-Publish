cb3f6fd2ce22f553a736090c1b86a3090b424234
Adrian Kunz <clashsoft@hotmail.com>
2019-03-13 19:48:35 +0100

    Update SimplePublishPluginTest

--- src/test/groovy/de/clashsoft/simplepublish/SimplePublishPluginTest.groovy
+++ src/test/groovy/de/clashsoft/simplepublish/SimplePublishPluginTest.groovy
@@ -1,25 +1,12 @@
 package de.clashsoft.simplepublish
 
 import org.gradle.api.Project
-import org.gradle.api.tasks.bundling.Jar
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
-import static org.junit.Assert.assertTrue
-
 class SimplePublishPluginTest {
 	@Test
-	void addsSourcesAndJavadocTask() {
-		Project project = ProjectBuilder.builder().build()
-		project.pluginManager.apply 'java'
-		project.pluginManager.apply 'de.clashsoft.simple-publish'
-
-		assertTrue(project.tasks.sourcesJar instanceof Jar)
-		assertTrue(project.tasks.javadocJar instanceof Jar)
-	}
-
-	@Test
-	void addsPublication() {
+	void supportsPublishInfo() {
 		Project project = ProjectBuilder.builder().withName('abc123').build()
 		project.pluginManager.apply 'java'
 		project.pluginManager.apply 'maven-publish'
