5dfe64aa0946b56378706de72932b528a89c9871
Adrian Kunz <clashsoft@hotmail.com>
2019-03-13 17:38:21 +0100

    publishInfo configuration
    - Added support for configuration via publishInfo.
    - publishing and bintray are now automatically configured from publishInfo.

--- src/main/groovy/de/clashsoft/simplepublish/SimplePublishPlugin.groovy
+++ src/main/groovy/de/clashsoft/simplepublish/SimplePublishPlugin.groovy
@@ -2,6 +2,7 @@ package de.clashsoft.simplepublish
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.tasks.bundling.Jar
 
 class SimplePublishPlugin implements Plugin<Project> {
@@ -10,6 +11,28 @@ class SimplePublishPlugin implements Plugin<Project> {
 		if (target.pluginManager.hasPlugin('java')) {
 			configureJava(target)
 		}
+
+		def publishInfo = target.extensions.create('publishInfo', PublishInfo)
+
+		target.afterEvaluate {
+			if (target.pluginManager.hasPlugin('maven-publish')) {
+				configureMaven(target, publishInfo)
+			}
+
+			if (target.pluginManager.hasPlugin('com.jfrog.bintray')) {
+				configureBintray(target, publishInfo)
+			}
+		}
+	}
+
+	private void complete(PublishInfo publishInfo) {
+		/*
+		if (publishInfo.githubRepo) {
+			publishInfo.issueTrackerUrl ?= "https://github.com/$publishInfo.githubRepo/issues/"
+			publishInfo.websiteUrl ?= "https://github.com/$publishInfo.githubRepo"
+			publishInfo.vcsUrl ?= "https://github.com/$publishInfo.githubRepo"
+		}
+		*/
 	}
 
 	private void configureJava(Project project) {
@@ -21,6 +44,91 @@ class SimplePublishPlugin implements Plugin<Project> {
 		project.task('javadocJar', type: Jar, dependsOn: 'javadoc') {
 			from project.javadoc.destinationDir
 			classifier = 'javadoc'
+		}
+	}
+
+	private void configureMaven(Project project, PublishInfo info) {
+		def pomConfig = {
+			licenses {
+				license {
+					name info.license.longName
+					url info.license.url
+				}
+			}
+
+			developers {
+				developer {
+					id info.developer.id
+					name info.developer.name
+					email info.developer.email
+				}
+			}
+
+			scm {
+				url info.vcsUrl
+			}
+		}
+
+		project.publishing {
+			publications {
+				"$project.name"(MavenPublication) {
+					from project.components.java
+					artifact project.tasks.sourcesJar
+					artifact project.tasks.javadocJar
+					groupId project.group
+					artifactId project.name
+					version project.version
+
+					pom.withXml {
+						def root = asNode()
+						root.appendNode('description', project.description)
+						root.appendNode('name', project.name)
+						root.appendNode('url', info.websiteUrl)
+						root.children().last() + pomConfig
+					}
+				}
+			}
+		}
+	}
+
+	private void configureBintray(Project project, PublishInfo info) {
+		project.bintray {
+			user = System.getenv('BINTRAY_USER')
+			key = System.getenv('BINTRAY_KEY')
+			publications = [ project.name ]
+			// dryRun = true
+			// publish = true
+			// override = true
+
+			pkg {
+				repo = 'maven'
+				name = project.name
+				// userOrg = ''
+				desc = project.description
+				websiteUrl = info.websiteUrl
+				issueTrackerUrl = info.issueTrackerUrl
+				vcsUrl = info.vcsUrl
+				licenses = [ info.license.shortName ]
+				labels = info.labels
+				publicDownloadNumbers = true
+				// attributes = []
+
+				githubRepo = info.githubRepo
+				githubReleaseNotesFile = 'CHANGELOG.md'
+
+				version {
+					name = project.version
+					desc = "$project.name v$project.version"
+					released = new Date()
+					vcsTag = "v$project.version"
+					// attributes = []
+
+					gpg {
+						sign = true
+						passphrase = System.getenv('BINTRAY_GPG_PASSPHRASE')
+					}
+				}
+			}
 		}
 	}
 }
--- src/test/groovy/de/clashsoft/simplepublish/SimplePublishPluginTest.groovy
+++ src/test/groovy/de/clashsoft/simplepublish/SimplePublishPluginTest.groovy
@@ -17,4 +17,35 @@ class SimplePublishPluginTest {
 		assertTrue(project.tasks.sourcesJar instanceof Jar)
 		assertTrue(project.tasks.javadocJar instanceof Jar)
 	}
+
+	@Test
+	void addsPublication() {
+		Project project = ProjectBuilder.builder().withName('abc123').build()
+		project.pluginManager.apply 'java'
+		project.pluginManager.apply 'maven-publish'
+		project.pluginManager.apply 'de.clashsoft.simple-publish'
+
+		project.group = 'com.example'
+		project.version = '1.2.3'
+		project.description = 'some project'
+		project.publishInfo {
+			websiteUrl = 'https://github.com/Clashsoft/abc123'
+			issueTrackerUrl = 'https://github.com/Clashsoft/abc123/issues'
+			vcsUrl = 'https://github.com/Clashsoft/abc123'
+			githubRepo = 'Clashsoft/abc123'
+			labels = [ 'source', 'generator', 'tree', 'ast' ]
+
+			license {
+				shortName 'BSD 3-Clause'
+				longName 'BSD 3-Clause "New" or "Revised" License'
+				url 'https://opensource.org/licenses/BSD-3-Clause'
+			}
+
+			developer {
+				id 'Clashsoft'
+				name 'Adrian Kunz'
+				email 'clashsoft@hotmail.com'
+			}
+		}
+	}
 }
