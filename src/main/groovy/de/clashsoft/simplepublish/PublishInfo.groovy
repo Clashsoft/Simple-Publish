cd41658be3bdf50e22c847bf5ca6de3c324bd1af
Adrian Kunz <clashsoft@hotmail.com>
2019-03-16 14:21:46 +0100

    Use Properties in PublishInfo
    * The PublishInfo class now uses Gradle Properties.

--- src/main/groovy/de/clashsoft/simplepublish/PublishInfo.groovy
+++ src/main/groovy/de/clashsoft/simplepublish/PublishInfo.groovy
@@ -2,22 +2,32 @@ package de.clashsoft.simplepublish
 
 import org.gradle.api.Action
 import org.gradle.api.Project
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.Property
 import org.gradle.api.publish.maven.MavenPomDeveloper
 import org.gradle.api.publish.maven.MavenPomLicense
 
 class PublishInfo {
 	private final Project project
 
-	String organization
-	String[] labels
+	final Property<String> organization
+	final ListProperty<String> labels
 
-	String websiteUrl
-	String issueTrackerUrl
-	String vcsUrl
-	String githubRepo
+	final Property<String> websiteUrl
+	final Property<String> issueTrackerUrl
+	final Property<String> vcsUrl
+	final Property<String> githubRepo
 
 	PublishInfo(Project project) {
 		this.project = project
+
+		organization = project.objects.property(String)
+		labels = project.objects.listProperty(String)
+
+		websiteUrl = project.objects.property(String)
+		issueTrackerUrl = project.objects.property(String)
+		vcsUrl = project.objects.property(String)
+		githubRepo = project.objects.property(String)
 	}
 
 	void license(Action<? super MavenPomLicense> action) {
