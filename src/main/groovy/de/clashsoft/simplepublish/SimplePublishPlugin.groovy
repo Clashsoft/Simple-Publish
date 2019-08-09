39b7cfcf0d5ddd0e227db8da3375f5f24d5f214d
Adrian Kunz <clashsoft@hotmail.com>
2019-08-10 00:48:46 +0200

    Static compilation
    * Some bintray properties are no longer configured incorrectly. #1

--- src/main/groovy/de/clashsoft/simplepublish/SimplePublishPlugin.groovy
+++ src/main/groovy/de/clashsoft/simplepublish/SimplePublishPlugin.groovy
@@ -1,101 +1,127 @@
 package de.clashsoft.simplepublish
 
+import com.jfrog.bintray.gradle.BintrayExtension
+import groovy.transform.CompileStatic
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPom
+import org.gradle.api.publish.maven.MavenPomScm
 import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.publish.maven.internal.publication.DefaultMavenPublication
+import org.gradle.api.tasks.SourceSetContainer
 import org.gradle.api.tasks.bundling.Jar
+import org.gradle.api.tasks.javadoc.Javadoc
 
+@CompileStatic
 class SimplePublishPlugin implements Plugin<Project> {
 	@Override
-	void apply(Project target) {
-		target.plugins.apply('java')
-		target.plugins.apply('maven-publish')
+	void apply(Project project) {
+		project.plugins.apply('java')
+		project.plugins.apply('maven-publish')
 
-		def publishInfo = target.extensions.create('publishInfo', PublishInfo, target)
+		createArtifactTasks(project)
 
-		configureArtifactTasks(target)
+		final PublishInfo publishInfo = project.extensions.create('publishInfo', PublishInfo, project)
 
-		target.publishing.publications.create(target.name, MavenPublication) {
-			from target.components.java
-			artifact target.tasks.sourcesJar
-			artifact target.tasks.javadocJar
+		final PublishingExtension publishing = project.extensions.getByType(PublishingExtension)
 
-			pom {
-				name = target.provider { target.name }
-				description = target.provider { target.description }
-				url = publishInfo.websiteUrl
-				scm {
-					url = publishInfo.vcsUrl
-				}
-			}
+		publishing.publications.create(project.name, MavenPublication) { MavenPublication pub ->
+			configureMaven(project, publishInfo, pub)
 		}
 
-		target.afterEvaluate {
-			target.publishing.publications."$target.name" {
-				// neither Project nor Publication uses the Property API for these
-				groupId = groupId ?: target.group
-				artifactId = artifactId ?: target.name
-				version = version ?: target.version
+		project.afterEvaluate {
+			publishing.publications.getByName(project.name) { MavenPublication pub ->
+				configureMavenAfter(project, pub)
 			}
 
-			if (target.pluginManager.hasPlugin('com.jfrog.bintray')) {
-				configureBintray(target, publishInfo)
+			if (project.pluginManager.hasPlugin('com.jfrog.bintray')) {
+				project.extensions.configure(BintrayExtension) { BintrayExtension bt ->
+					configureBintrayAfter(bt, project, publishInfo)
+				}
 			}
 		}
 	}
 
-	private void configureArtifactTasks(Project project) {
+	private static void createArtifactTasks(Project project) {
 		project.tasks.register('sourcesJar', Jar) {
-			it.from project.sourceSets.main.allSource
-			it.classifier = 'sources'
+			final SourceSetContainer sourceSets = project.convention.getPlugin(JavaPluginConvention).sourceSets
+
+			it.from sourceSets.getByName('main').allSource
+			it.archiveClassifier.set 'sources'
 		}
 
 		project.tasks.register('javadocJar', Jar) {
-			it.dependsOn 'javadoc'
-			it.from project.javadoc.destinationDir
-			it.classifier = 'javadoc'
+			final Javadoc javadoc = project.tasks.getByName('javadoc') as Javadoc
+
+			it.dependsOn javadoc
+			it.from javadoc.destinationDir
+			it.archiveClassifier.set 'javadoc'
 		}
 	}
 
-	private void configureBintray(Project project, PublishInfo info) {
-		project.bintray {
-			user = user ?: project.findProperty('bintray.user') ?: System.getenv('BINTRAY_USER')
-			key = key ?: project.findProperty('bintray.key') ?: System.getenv('BINTRAY_KEY')
-			publications = publications ?: [ project.name ]
-			// dryRun = true
-			// publish = true
-			// override = true
+	private static void configureMaven(Project project, PublishInfo publishInfo, MavenPublication pub) {
+		pub.from project.components.getByName('java')
+		pub.artifact project.tasks.getByName('sourcesJar')
+		pub.artifact project.tasks.getByName('javadocJar')
 
-			pkg {
-				repo = repo ?: 'maven'
-				name = name ?: project.name
-				userOrg = userOrg ?: info.organization ?: null
-				desc = desc ?: project.description
-				websiteUrl = websiteUrl ?: info.websiteUrl
-				issueTrackerUrl = issueTrackerUrl ?: info.issueTrackerUrl
-				vcsUrl = vcsUrl ?: info.vcsUrl
-				licenses = licenses ?: project.publishing.publications[project.name].pom.licenses*.name
-				labels = labels ?: info.labels
-				publicDownloadNumbers = true
-				// attributes = []
+		final MavenPom pom = pub.pom
 
-				githubRepo = githubRepo ?: info.githubRepo
-				githubReleaseNotesFile = githubReleaseNotesFile ?: 'CHANGELOG.md'
-
-				version {
-					name = name ?: project.version
-					desc = desc ?: "$project.name v$project.version"
-					released = released ?: new Date()
-					vcsTag = vcsTag ?: "v$project.version"
-					// attributes = []
-
-					gpg {
-						sign = true
-						passphrase = passphrase ?: project.findProperty('bintray.gpg.passphrase')
-								?: System.getenv('BINTRAY_GPG_PASSPHRASE')
-					}
-				}
-			}
+		pom.name.set project.provider { project.name }
+		pom.description.set project.provider { project.description }
+		pom.url.set publishInfo.websiteUrl
+		pom.scm { MavenPomScm scm ->
+			scm.url.set publishInfo.vcsUrl
 		}
+	}
+
+	private static void configureMavenAfter(Project project, MavenPublication pub) {
+		// neither Project nor Publication uses the Property API for these
+		pub.groupId = pub.groupId ?: project.group
+		pub.artifactId = pub.artifactId ?: project.name
+		pub.version = pub.version ?: project.version
+	}
+
+
+	private static void configureBintrayAfter(BintrayExtension bt, Project project, PublishInfo info) {
+		final PublishingExtension publishing = project.extensions.getByType(PublishingExtension)
+		final DefaultMavenPublication publication = publishing.publications[project.name] as DefaultMavenPublication
+
+		bt.user = bt.user ?: project.findProperty('bintray.user') ?: System.getenv('BINTRAY_USER')
+		bt.key = bt.key ?: project.findProperty('bintray.key') ?: System.getenv('BINTRAY_KEY')
+		bt.publications = bt.publications ?: [ project.name ] as String[]
+		// bt.dryRun = true
+		// bt.publish = true
+		// bt.override = true
+
+		final BintrayExtension.PackageConfig pkg = bt.pkg
+		pkg.repo = pkg.repo ?: 'maven'
+		pkg.name = pkg.name ?: project.name
+		pkg.userOrg = pkg.userOrg ?: info.organization.get() ?: null
+		pkg.desc = pkg.desc ?: project.description
+		pkg.websiteUrl = pkg.websiteUrl ?: info.websiteUrl.get()
+		pkg.issueTrackerUrl = pkg.issueTrackerUrl ?: info.issueTrackerUrl.get()
+		pkg.vcsUrl = pkg.vcsUrl ?: info.vcsUrl.get()
+		pkg.licenses = pkg.licenses ?: publication.pom.licenses*.name as String[]
+		pkg.labels = pkg.labels ?: info.labels.get() as String[]
+		pkg.publicDownloadNumbers = true
+		// pkg.attributes = []
+
+		pkg.githubRepo = pkg.githubRepo ?: info.githubRepo.get()
+		pkg.githubReleaseNotesFile = pkg.githubReleaseNotesFile ?: 'CHANGELOG.md'
+
+		final BintrayExtension.VersionConfig ver = pkg.version
+
+		ver.name = ver.name ?: project.version
+		ver.desc = ver.desc ?: "$project.name v$project.version"
+		ver.released = ver.released ?: new Date()
+		ver.vcsTag = ver.vcsTag ?: "v$project.version"
+		// ver.attributes = []
+
+		final BintrayExtension.GpgConfig gpg = ver.gpg
+		gpg.sign = true
+		gpg.passphrase = gpg.passphrase ?: project.findProperty('bintray.gpg.passphrase')
+				?: System.getenv('BINTRAY_GPG_PASSPHRASE')
 	}
 }
